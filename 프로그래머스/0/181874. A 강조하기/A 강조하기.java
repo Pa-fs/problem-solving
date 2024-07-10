93a9ec92ea6dd5a677513800af5eaf592929e6c5
Pa-fs <162409934+Pa-fs@users.noreply.github.com>
2024-07-10 15:00:21 +0900

    [level 0] Title: A 강조하기, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -1,17 +1,7 @@
 class Solution {
     public String solution(String myString) {
-        String answer = "";
-        for(int i = 0; i < myString.length(); i++) {
-            char ch = myString.charAt(i);
-            if(ch == 'a') {
-                answer = answer + (char)(ch - ('a' - 'A'));
-            } else {
-                if(ch >= 'B' && ch <= 'Z') {
-                    answer = answer + (char)(ch + ('a' - 'A'));
-                }
-                else answer = answer + ch;
-            }
-        }
-        return answer;
+        myString = myString.toLowerCase();
+        myString = myString.replace('a', 'A');
+        return myString;
     }
 }
