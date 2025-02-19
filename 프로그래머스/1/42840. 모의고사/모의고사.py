a9ced67422f9c1753bc672bc2d9fa86ff7154654
Pa-fs <162409934+Pa-fs@users.noreply.github.com>
2025-02-19 17:49:05 +0900

    [level 1] Title: 모의고사, Time: 7.26 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,19 +1,20 @@
 def solution(answers):
-    answer = []
+    lst = []
     
-    # 수포자 찍기리스트
-    random_list = [[1,2,3,4,5,], [2,1,2,3,2,4,2,5,], [3,3,1,1,2,2,4,4,5,5]]
+    person = [[1,2,3,4,5], [2,1,2,3,2,4,2,5], [3,3,1,1,2,2,4,4,5,5,]]
+    cnt = [0] * 3
     
-    # 정답개수 구하기
-    person = [0] * 3
-    for i in range(len(answers)):
-        for j in range(len(random_list)):
-            if answers[i] == random_list[j][i % len(random_list[j])]:
-                person[j] += 1
+    for i, answer in enumerate(answers):
+        for j in range(len(person)):
+            if answer == person[j][i % len(person[j])]:
+                cnt[j] += 1
     
-    # 정답 개수 여럿일 경우 처리
-    maxval = max(person)
-    for i in range(len(person)):
-        if person[i] >= maxval:
-            answer.append(i + 1)
-    return answer
+    maxx = -1
+    for i, val in enumerate(cnt):
+        if maxx < val:
+            maxx = val
+
+    for i, val in enumerate(cnt):
+        if maxx == val:
+            lst.append(i + 1)
+    return lst
