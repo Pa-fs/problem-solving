07af5e0b1c352e5c0c09e9a045ad9a0e38157d7a
Pa-fs <162409934+Pa-fs@users.noreply.github.com>
2025-03-05 14:45:35 +0900

    [Silver II] Title: 부분수열의 합, Time: 376 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -0,0 +1,31 @@
+res = 0
+def go(nums, chk, n, s, lev, sum, tmp_lst, start):
+    global res
+    if len(tmp_lst) > n:
+        return
+
+    if len(tmp_lst) > 0 and sum == s:
+        # print(tmp_lst)
+        res += 1
+        # return 조심
+
+    for i in range(start, n):
+        if chk[i]:
+            continue
+
+        chk[i] = True
+        tmp_lst.append(nums[i])
+        go(nums, chk, n, s, lev + 1, sum + nums[i], tmp_lst, i + 1)
+        tmp_lst.pop()
+        chk[i] = False
+
+
+def solution():
+    n, s = map(int, input().split())
+    nums = list(map(int, input().split()))
+
+    chk = [False] * n
+
+    go(nums, chk, n, s, 0, 0,[], 0)
+    print(res)
+solution()
