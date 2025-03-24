e15a622a59345429ff4bda8822aa3528582d986b
Pa-fs <162409934+Pa-fs@users.noreply.github.com>
2025-03-24 16:13:23 +0900

    [Silver V] Title: 날짜 계산, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.py
@@ -0,0 +1,21 @@
+e,s,m = map(int, input().split())
+
+def Solution():
+    year = 1
+    a, b, c = 1, 1, 1
+    while True:
+        if e == a and s == b and m == c:
+            break
+
+        a += 1
+        if a > 15:
+            a = 1
+        b += 1
+        if b > 28:
+            b = 1
+        c += 1
+        if c > 19:
+            c = 1
+        year += 1
+    print(year)
+Solution()
