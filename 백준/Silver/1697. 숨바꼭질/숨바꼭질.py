c0dac9c94f2952b34f88ea432d9c06fcec494979
Pa-fs <162409934+Pa-fs@users.noreply.github.com>
2025-03-26 20:19:07 +0900

    [Silver I] Title: 숨바꼭질, Time: 112 ms, Memory: 42756 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,30 @@
+from collections import deque
+n, k = map(int, input().split())
+dx = [-1, 1, 2]
+vis = [0] * 200001
+dist = [0] * 200001
+def bfs():
+    global n, k
+    q = deque()
+    q.append(n)
+    vis[n] = 1
+    while q:
+        cur = q.popleft()
+        if cur == k:
+            break
+        for i in range(len(dx)):
+            if dx[i] == 2:
+                nx = cur * dx[i]
+            else:
+                nx = cur + dx[i]
+            if nx < 0 or nx > 100001:
+                continue
+            if vis[nx]:
+                continue
+            vis[nx] = vis[cur] + 1
+            dist[nx] = dist[cur] + 1
+            q.append(nx)
+    print(dist[k])
+def Solution():
+    bfs()
+Solution()
